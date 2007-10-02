f9dac6087dc40429b0cccdf79abe8726b650a97a
Yann Ramin <atrus@stackworks.net>
2007-10-02 15:53:22 -0700

    Some more functional testing - space and other characters seem broken
    Signed-off-by: Yann Ramin <atrus@stackworks.net>

--- softwedge.c
+++ softwedge.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/XTest.h>
+#include <X11/keysymdef.h>
 
 
-void key_press(Display *dpy, unsigned int keycode) {
+void key_press(Display *dpy, unsigned char letter) {
+  unsigned int shiftcode = XKeysymToKeycode(dpy, XStringToKeysym("Shift_L"));
+
+  char s[2];
+  s[0] = letter;
+  s[1] = 0;
+  KeySym sym = XStringToKeysym(s);
+  KeyCode keycode = XKeysymToKeycode(dpy, sym);
+  
+  if (isupper(letter))
+    XTestFakeKeyEvent(dpy, shiftcode, True, 0);
+  
   XTestFakeKeyEvent(dpy, keycode, True, 0);	
-  XTestFakeKeyEvent(dpy, keycode, False, 0);	
+  XTestFakeKeyEvent(dpy, keycode, False, 0);
+
+  if (isupper(letter))
+    XTestFakeKeyEvent(dpy, shiftcode, False, 0);	
   
   XFlush(dpy);
+}
+
+void press_keys(Display *dpy, char* string) {
+  int len = strlen(string);
+  int i = 0;
+  for (i = 0; i < len; i++) {
+    key_press(dpy, string[i]);
+  }
 }
 
 int main(int argc, char**argv)
@@ -39,17 +63,12 @@ int main(int argc, char**argv)
     }
   
 
-
-  KeySym sym = XStringToKeysym("a");
-  KeySym lower;
-  KeySym upper;
-  XConvertCase(sym, &lower, &upper);
-  KeyCode code = XKeysymToKeycode(dpy, upper);
-  key_press(dpy, code);
+  press_keys(dpy, "Hello world!");
 
 
-  code = XKeysymToKeycode(dpy, lower);
-  key_press(dpy, code);
+
+
+
 
   return 0;
 }
